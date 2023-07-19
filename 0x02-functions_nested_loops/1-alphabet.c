67f34e55bdfa268cd79b0d0f67514a43b00be3c0
iyiola LADEJO <81442011+iyioladejo3@users.noreply.github.com>
2023-07-19 14:04:49 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - print_alphabet
- * Return: 0
+ * print_alphabet - Entry point
+ * Return:0
  */
 
 void print_alphabet(void)
@@ -16,4 +16,5 @@ void print_alphabet(void)
 
 	}
 	_putchar('\n');
+
 }
