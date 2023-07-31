f2fe5afde6187a2d899be9f24f05d64f4a76e1d9
iyiola LADEJO <81442011+iyioladejo3@users.noreply.github.com>
2023-07-31 14:50:46 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 /**
  * _memset - fills memory with a constant byte
- * @s: pointer to the block of memory to fill
- * @b: value to set
- * @n: number of bytes to fill
- * Return: pointer to the memory block 's'
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
     unsigned int i;
 
-    /* Using a FOR loop to fill the memory block */
     for (i = 0; i < n; i++)
     {
-        *(s + i) = b; /* Fill one position at a time */
+        s[i] = b;
     }
 
     return (s);
