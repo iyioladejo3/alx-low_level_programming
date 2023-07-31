2043cddb3eb0c93a6273783f9a367d3ce96d67cb
iyiola LADEJO <81442011+iyioladejo3@users.noreply.github.com>
2023-07-31 14:59:12 +0200

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,3 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
-int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -11,5 +7,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
+int _putchar(char c);
